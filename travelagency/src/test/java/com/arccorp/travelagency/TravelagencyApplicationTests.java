2075c1a0b35c54edaffdad056a51bf4845829039
Steve Botham <sbotham1968@gmail.com>
2018-09-19 14:48:34 -0500

    1

--- travelagency/src/test/java/com/arccorp/travelagency/TravelagencyApplicationTests.java
+++ travelagency/src/test/java/com/arccorp/travelagency/TravelagencyApplicationTests.java
@@ -2,15 +2,37 @@ package com.arccorp.travelagency;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+
 public class TravelagencyApplicationTests {
 
+	
+	Logger log = LoggerFactory.getLogger(TravelagencyApplicationTests.class);
+	
+	
+	@Autowired
+	public TravelAgencyRepository travelAgencyRepository;
+	
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void getTravelAgencies() {
+		String mName="Tests.getTravelAgencies";
+		
+		for (TravelAgency ta : travelAgencyRepository.findAll()) {
+			if (log.isDebugEnabled()) {
+				log.debug(mName+" "+ta.toString());
+			}
+		}
 	}
 
 }
